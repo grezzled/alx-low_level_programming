dd86140f0efff988aead40feab338ab314d582b6
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-06 20:03:27 +0100

    new commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ * Description: This function print alphabets
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+if (a != '9')
+{
+putchar(',');
+}
+putchar(' ');
+}
+
+putchar('\n');
+
+return (0);
+}
