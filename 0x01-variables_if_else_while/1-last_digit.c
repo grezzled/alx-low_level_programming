41f503e60731b5fd29e6384828678d94a68c66ff
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-06 19:14:21 +0100

    new commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,18 +5,17 @@
 
 /**
  * main - Entry point of the program
- * Description: This function prints
+ * Description: This function printspush
  * Return: Always 0
  */
 
 int main(void)
 {
 int n;
-
+int lastDigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
-int lastDigit = n % 10;
+lastDigit = n % 10;
 
 if (n == 0)
 {
