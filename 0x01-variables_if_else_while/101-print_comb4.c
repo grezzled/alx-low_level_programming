b14e3fa67fd2f38f940c2a3212c71cfc29687dc0
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-06 20:46:29 +0100

    new commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ * Description: This function print alphabets
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int a;
+int b;
+int c;
+for (a = '0'; a <= '7'; a++)
+{
+for (b = a + 1; b <= '8'; b++)
+{
+for (c = b + 1; c <= '9'; c++)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if (a != '7' || b != '8' || c != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+
+putchar('\n');
+
+return (0);
+}
