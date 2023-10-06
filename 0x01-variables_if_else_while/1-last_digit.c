b49a722b7951006158f9d5ce661159a4fd7e6997
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-06 19:22:01 +0100

    new commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,11 +17,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastDigit = n % 10;
 
-if (n == 0)
+if (lastDigit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 }
-else if (n < 6)
+else if (lastDigit < 6)
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 }
