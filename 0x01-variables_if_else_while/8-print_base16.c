3dd50d5c22ddd9e4fa6f74b71aee04f097df80d1
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-06 19:56:59 +0100

    new commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point of the program
+ * Description: This function print alphabets
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char a;
+for (a = '0'; a <= '9'; a++)
+{
+putchar(a);
+}
+for (a = 'a'; a <= 'f'; a++)
+{
+putchar(a);
+}
+putchar('\n');
+
+return (0);
+}
