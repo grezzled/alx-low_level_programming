48266838d1399bfb8fd71961d4a63d67b4b8d709
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-10 19:20:11 +0100

    new

---
+++ 0x03-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description: This function prints _putchar
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("_putchar");
+return (0);
+}
