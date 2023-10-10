ec9f84ec2b5d86e14f3ad22be31ce9da4f40d3d8
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-10 20:06:50 +0100

    new commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,10 +2,12 @@
 
 /**
  * _islower - This function checks if it's lower case
+ * @c: character to  check.
+ * return: true if it's lower
  */
 int _islower(int c)
 {
-if(c <= 'z' && c >= 'a')
+if (c <= 'z' && c >= 'a')
 {
 return (1);
 }
