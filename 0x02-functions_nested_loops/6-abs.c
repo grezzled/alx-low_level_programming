ba3dfcc92b1e1361e3ce979fa2f03f7de38db89f
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-10 20:24:59 +0100

    new commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs -  Prints the abs of a number.
+ * @n: number to  check.
+ * Return: number
+ */
+int _abs(int n)
+{
+int value = n;
+if (value<0)
+{
+value = -n;
+}
+return (value)
+}
