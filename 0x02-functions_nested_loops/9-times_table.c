baae645f9beb9f7a51e285acb951553b58862836
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-11 00:15:09 +0100

    new commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,15 +9,15 @@ void times_table(void)
 int i, j;
 for (i = 0; i <= 9; i++)
 {
-__putchar('0');
-__putchar(',');
-__putchar(' ');
+_putchar('0');
+_putchar(',');
+_putchar(' ');
 for (j = 1; j <= 9; j++)
 {
 int p = i * j;
 if (p <= 9)
 _putchar(' ');
-else if ( p>9 )
+else if (p > 9)
 _putchar((p / 10) + '0');
 _putchar((p % 10) + '0');
 if (j < 9)
