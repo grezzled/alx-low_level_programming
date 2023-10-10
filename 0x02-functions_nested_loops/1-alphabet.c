37aa8460cbe68bb4ed10413f5b1c11f9c7d9726c
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-10 19:47:39 +0100

    new commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point of the program
  * print_alphabet - This function prints the alphabet in lower case
- * Return: Always 0
  */
 void print_alphabet(void)
 {
