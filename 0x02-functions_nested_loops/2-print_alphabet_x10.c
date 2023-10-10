e8c76cacc40011c37c0925c8eac194b419eb3380
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-10 19:50:42 +0100

    new commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - This function prints the alphabet in lower case
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for(i = 0; i <10; i++){
+char a;
+for (a = 'a'; a <= 'z'; a++)
+_putchar(a);
+}
+_putchar('\n');
+}
