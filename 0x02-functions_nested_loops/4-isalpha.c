b56602c2197e4f8b4d6d2f539f196e394980e764
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-10 20:09:28 +0100

    new commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - This function checks if it's lower case
+ * @c: character to  check.
+ * Return: true if it's alpha
+ */
+int _isalpha(int c)
+{
+if (c <= 'z' && c >= 'a' || c <= 'Z' && c >= 'A')
+{
+return (1);
+}
+else
+return (0);
+}
