9594c0463f32fa0fb378c44856e0d6213e358125
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-12 10:12:55 +0100

    new commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - A function that returns the upper case string
+ * @c: character to check
+ * Return:  1 | 0
+ */
+void _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
