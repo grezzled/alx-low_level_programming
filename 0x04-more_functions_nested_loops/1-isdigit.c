07b9a4b9b35534ca4b2d910395afaf222b022967
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-12 10:30:34 +0100

    new commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+/**
+ * _isupper - A function that returns if it's digit
+ * @c: character to check
+ * Return:  1 | 0
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
