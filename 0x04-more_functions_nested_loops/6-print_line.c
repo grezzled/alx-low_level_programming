97bc7c0e4c9bc2b1533021e936749ef175dd37eb
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-12 21:59:29 +0100

    new commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line -	multiplies two integers.
+ * @n: is the number of times the character	_ should be printed
+ */
+void print_line(int	n)
+{
+	int	i;
+
+	for	(i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
