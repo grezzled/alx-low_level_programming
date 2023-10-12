3499e24adc5791026367e3c770d28d067bff2bd2
grezzled <belchhabsoufiane.official@gmail.com>
2023-10-12 22:09:13 +0100

    new commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_square	-	multiplies two integers.
+ * @n: is the number of times the character	# should be printed
+ */
+void print_square(int n)
+{
+	int	row, column;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for	(row = 0; row <	size; row++)
+		{
+			for	(column	= 0; column	< size;	column++)
+			{
+				_putchar(35); // '#' character
+			}
+			_putchar('\n');
+		}
+	}
+}
